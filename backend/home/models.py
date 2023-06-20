3203025e341f7d13ac52fa2b11a5ee10dda339d0
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-20 19:12:24 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,8 +1,5 @@
 from django.conf import settings
 from django.db import models
-class Hello(models.Model):
-    'Generated Model'
-    fssddsd = models.BigIntegerField()
 class Hi(models.Model):
     'Generated Model'
     kdggf = models.BigIntegerField()
