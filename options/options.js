2f7fa7d84f76b68d32815ddc5d6488531671ae1b
Crowdbotics <engineering@crowdbotics.com>
2023-06-20 18:29:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "memory-exceeded-qco-93458",
+  url: "https://memory-exceeded-qco-93458.botics.co",
+  api: "https://memory-exceeded-qco-93458.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
